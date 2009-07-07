4bba138994ecc88cb430e6bfe89b62465423a5f2
Richard Moore <rich@kde.org>
2009-07-07 11:56:43 +0100

    - Added a bit more information in a comment.

--- qt-bugs/qregexp-bug/main.cpp
+++ qt-bugs/qregexp-bug/main.cpp
@@ -1,6 +1,10 @@
 #include <qregexp.h>
 #include <stdio.h>
 
+/**
+ * This regexp is invalid as the quantifier should only work inside
+ * the group. Instead the regexp is treated as valid.
+ */
 int main( int argc, char **argv )
 {
     // QRegExp re( "on_([\\da-zA-Z])+_([\\da-zA-Z])+" ); // QRegExp bug (quantifier is invalid)
