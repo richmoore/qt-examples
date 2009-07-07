ea6f78498b514b2a7203173bbf23b6e794e4b04a
Richard Moore <rich@kde.org>
2009-07-07 10:34:29 +0100

    - Handle command line args and provide usage information.

--- qtscript-ui/main.cpp
+++ qtscript-ui/main.cpp
@@ -1,17 +1,26 @@
 #include <qapplication.h>
 #include <qwidget.h>
 
+#include <stdio.h>
+
 #include "scriptui.h"
 
 int main( int argc, char **argv )
 {
     QApplication app( argc, argv );
 
+    if ( argc != 2 ) {
+	printf( "Usage: %s <nameprefix>\n", argv[0] );
+	return 0;
+    }
+
+    QString prefix = QString::fromLocal8Bit( argv[1] );
+
     ScriptUi ui;
-    if ( !ui.loadScript( "converter.js") )
+    if ( !ui.loadScript( QString("%1.js").arg(prefix) ) )
 	return false;
 
-    if ( !ui.loadUi( "converter.ui") )
+    if ( !ui.loadUi( QString("%1.ui").arg(prefix) ) )
 	return false;
 
     ui.connectScript();
