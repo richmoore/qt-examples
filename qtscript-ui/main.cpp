af99334fc8cefc10ec0b1b116baacc16ac11a90e
Richard Moore <rich@kde.org>
2009-07-07 11:23:23 +0100

    - Add support for specifying either the prefix, or the path to the individual   ui file and js file

--- qtscript-ui/main.cpp
+++ qtscript-ui/main.cpp
@@ -9,18 +9,30 @@ int main( int argc, char **argv )
 {
     QApplication app( argc, argv );
 
-    if ( argc != 2 ) {
-	printf( "Usage: %s <nameprefix>\n", argv[0] );
+    QString scriptName;
+    QString uiName;
+
+    if ( (argc != 2) && (argc != 3) ) {
+	printf( "Usage: %s <nameprefix>\n"
+                "       %s <uifile> <jsfile>\n",
+		argv[0], argv[0] );
 	return 0;
     }
-
-    QString prefix = QString::fromLocal8Bit( argv[1] );
+    else if ( argc == 2 ) {
+	QString prefix = QString::fromLocal8Bit( argv[1] );
+	scriptName = QString("%1.js").arg(prefix);
+	uiName = QString("%1.ui").arg(prefix);
+    }
+    else {
+	uiName = QString::fromLocal8Bit( argv[1] );
+	scriptName = QString::fromLocal8Bit( argv[2] );
+    }
 
     ScriptUi ui;
-    if ( !ui.loadScript( QString("%1.js").arg(prefix) ) )
+    if ( !ui.loadScript( scriptName ) )
 	return false;
 
-    if ( !ui.loadUi( QString("%1.ui").arg(prefix) ) )
+    if ( !ui.loadUi( uiName ) )
 	return false;
 
     ui.connectScript();
